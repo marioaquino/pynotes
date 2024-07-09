e3ddfe2f7c2277ccf4c89910cf460f3d8c39a0a4
Luis <liaquino122@gmail.com>
2024-07-09 19:21:46 -0400

    learned about http verbs

--- helloballs.py
+++ helloballs.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, popo!</p>"
+
+@app.get("/foo")
+def foo_get():
+    return "<p>This is foo</p>"
+
+@app.post("/foo")
+def foo_post():
+    return "<p>This is marios little balls</p>"
